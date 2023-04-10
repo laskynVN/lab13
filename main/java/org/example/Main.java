549d9ddcd3d23ad8a423a16ed0a9641cf48be3d4
laskynVN <nikita.volyntsevv@gmail.com>
2023-04-10 10:25:20 +0300

    task1

---
+++ main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Tester tester = new Tester("Nikita", "Volyntsev", 2, "B2", 1300);
+        tester.getFullName();
+        tester.print();
+    }
+}
